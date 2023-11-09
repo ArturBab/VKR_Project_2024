c28dcf5b61ee0abe5566daea46e3a61edc0a0c0c
ArturBab <90608148+ArturBab@users.noreply.github.com>
2023-11-10 00:08:32 +0300

    Update views.py

--- vkr/vkr_project/views.py
+++ vkr/vkr_project/views.py
@@ -1,6 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+import mimetypes
+from rest_framework.parsers import FileUploadParser
+from django.core.exceptions import ValidationError
+from django.http import HttpResponseBadRequest
 
 from .models import *
 from .serializers import *
@@ -17,11 +21,19 @@ def content_list(request):
         serializer = ContentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            content_file = request.FILES.get('content_file')
+            if content_file:
+                ###
+                # Сохранение файла в модели Content
+                content = serializer.instance
+                content.content_file = content_file
+                content.save()
+
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['DELETE'])
+@api_view(['GET', 'DELETE'])
 def content_delete(request):
     Content.objects.all().delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
@@ -38,7 +50,23 @@ def audio_files_list(request):
         serializer = AudioFilesSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            audio_file = request.FILES.get('audio_file')
+            if audio_file:
+
+                # Проверка формата файла
+                file_format, _ = mimetypes.guess_type(audio_file.name)
+                if not file_format or not file_format.startswith('audio'):
+                    return Response({'error': 'Invalid audio file format.'}, status=status.HTTP_400_BAD_REQUEST)
+
+                # Сохранение файла в модели Audio
+                audio = serializer.instance
+                audio.audio_file = audio_file
+                audio.save()
+
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+            return Response({'error': 'Audio file is required.'}, status=status.HTTP_400_BAD_REQUEST)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -52,14 +80,42 @@ def audio_files_delete(request):
 def video_files_list(request):
     if request.method == 'GET':
         video_files = VideoFiles.objects.all()
-        serializer = VideoFilesSerializer(video_files, many=True)
+        serializer = VideoFilesSerializer(
+            video_files, many=True, context={'request': request})
         return Response(serializer.data)
 
     elif request.method == 'POST':
-        serializer = VideoFilesSerializer(data=request.data)
+        serializer = VideoFilesSerializer(
+            data=request.data, context={'request': request})
         if serializer.is_valid():
-            serializer.save()
+            print(request.FILES)
+            video_file = request.FILES.get('video_file')
+            if not video_file:
+                return Response({'error': 'Video file is required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Проверка формата файла
+            allowed_formats = ['mp4', 'mkv', 'wav']
+            file_extension = video_file.name.split(
+                '.')[-1].lower() if '.' in video_file.name else None
+            print(f'File extension: {file_extension}')
+            print(f'Allowed formats: {allowed_formats}')
+            if file_extension not in allowed_formats:
+                print('Invalid format!')
+                return Response({'error': f'Invalid video file format. Allowed formats: {", ".join(allowed_formats)}.'},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            content_id = request.data.get('content_id')
+            # Проверяем, существует ли контент с переданным content_id
+            try:
+                content = Content.objects.get(pk=content_id)
+            except Content.DoesNotExist:
+                return Response({'error': 'Invalid content_id. Content does not exist.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Сохранение файла в модели Video
+            serializer.save(video_file=video_file, content_id=content)
+
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
