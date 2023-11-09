573961c4024ae0de4d5f42d43e3a6e63653c3e52
ArturBab <90608148+ArturBab@users.noreply.github.com>
2023-11-10 00:01:36 +0300

    Update models.py

--- vkr/vkr_project/models.py
+++ vkr/vkr_project/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Content(models.Model):
     title = models.CharField(max_length=255)
     file_path = models.CharField(max_length=255)
+    content_file = models.FileField(upload_to='content_files/')
 
 
 class VideoFiles(models.Model):
@@ -11,6 +12,7 @@ class VideoFiles(models.Model):
     description = models.CharField(max_length=255)
     file_path = models.CharField(max_length=255)
     format = models.CharField(max_length=255)
+    video_file = models.FileField(upload_to='video_files/')
     content_id = models.ForeignKey(
         Content, on_delete=models.CASCADE, related_name='video_files')
 
@@ -21,6 +23,7 @@ class AudioFiles(models.Model):
     data_create = models.CharField(max_length=255)
     file_path = models.CharField(max_length=255)
     format = models.CharField(max_length=255)
+    audio_file = models.FileField(upload_to='audio_files/')
     content_id = models.ForeignKey(
         Content, on_delete=models.CASCADE, related_name='audio_file')
     video_id = models.OneToOneField(
