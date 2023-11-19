4b27f41e6955a3fce56d3539aaf5f2a6becd7742
ArturBab <90608148+ArturBab@users.noreply.github.com>
2023-11-19 17:50:26 +0300

    Update urls.py

--- vkr/settings/urls.py
+++ vkr/settings/urls.py
@@ -6,14 +6,23 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api/content/', views.content_list),
-    path('content/delete/', views.content_delete, name='content-delete'),
+    path('api/content/update/<int:content_id>/',
+         views.update_content, name='update_content'),
+    path('api/content/delete/<int:audio_id>/',
+         views.content_delete, name='content-delete'),
+
     path('api/audio_files/', views.audio_files_list),
-    path('audio/delete/', views.audio_files_delete, name='audio-files-delete'),
+    path('api/audio/update/<int:audio_id>/',
+         views.update_audio_file, name='update_audio_file'),
+    path('api/audio/delete/', views.audio_files_delete, name='audio-files-delete'),
+
     path('api/video_files/', views.video_files_list),
-    path('video/delete/', views.video_files_delete, name='video-files-delete'),
-    path('prioritize-content/<int:video_id>/<int:audio_id>/',
-         views.prioritize_content, name='prioritize_content'),
+    path('api/video/update/<int:video_id>/',
+         views.update_video_file, name='update_video_file'),
+    path('api/video/delete/<int:video_id>/',
+         views.video_files_delete, name='video-files-delete'),
 ]
 
 if settings.DEBUG:
