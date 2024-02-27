b8bec669fbc8a4207b2780177f808bb6911c479f
Артур <90608148+ArturBab@users.noreply.github.com>
2024-02-27 14:58:07 +0300

    Update models.py

--- vkr/vkr_project/models.py
+++ vkr/vkr_project/models.py
@@ -2,12 +2,54 @@ from django.db import models
 import time
 from django.http import HttpResponse
 import os
+from django.contrib.auth.models import AbstractUser
+
+
+class Role(models.Model):
+    role = models.CharField(max_length=255)
+    is_root = models.BooleanField()
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=255, unique=True)
+    roles = models.ManyToManyField(Role)
+
+    def __str__(self):
+        return self.username
+
+
+class Student(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='student')
+    name = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    stud_id = models.CharField(max_length=10)
+    stud_group = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.name} {self.lastname} {self.stud_group}"
+
+
+class Teacher(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='teacher')
+    name = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    teach_id = models.CharField(max_length=10)
+    discipline = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name, self.lastname, self.discipline
 
 
 class Content(models.Model):
     title = models.CharField(max_length=255)
     file_path = models.CharField(max_length=255)
     content_file = models.FileField(upload_to='content_files/')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='contents')
 
     def __str__(self):
         return self.title
@@ -19,13 +61,15 @@ class VideoFiles(models.Model):
     file_path = models.CharField(max_length=255)
     format = models.CharField(max_length=255)
     video_file = models.FileField(upload_to='video_files/')
-    bandwidth = models.FloatField(null=True, blank=True)  # Определяет пропускную способность видеофайла
+    # Определяет пропускную способность видеофайла
+    bandwidth = models.FloatField(null=True, blank=True)
     content_id = models.ForeignKey(
         Content, on_delete=models.CASCADE, related_name='video_files')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='videos')
 
     def __str__(self):
         return self.title_video
-    
 
     def update_bandwidth_video(self):
         try:
@@ -44,7 +88,8 @@ class VideoFiles(models.Model):
 
             # Подсчет пропускной способности в Mbps
             file_size_in_bytes = os.path.getsize(video_path)
-            bandwidth = (file_size_in_bytes * 8) / (duration * 1000000)  # Переводим в Mbps
+            bandwidth = (file_size_in_bytes * 8) / \
+                (duration * 1000000)  # Переводим в Mbps
 
             # Обновление поля пропускной способности
             self.bandwidth = bandwidth
@@ -52,8 +97,7 @@ class VideoFiles(models.Model):
 
             return True  # Успешно обновлено
         except FileNotFoundError:
-            return False  # Ошибка: файл не найден    
-
+            return False  # Ошибка: файл не найден
 
 
 class AudioFiles(models.Model):
@@ -63,12 +107,15 @@ class AudioFiles(models.Model):
     file_path = models.CharField(max_length=255)
     format = models.CharField(max_length=255)
     audio_file = models.FileField(upload_to='audio_files/')
-    bandwidth = models.FloatField(null=True, blank=True)  # Определяет пропускную способность аудиофайла
+    # Определяет пропускную способность аудиофайла
+    bandwidth = models.FloatField(null=True, blank=True)
     content_id = models.ForeignKey(
         Content, on_delete=models.CASCADE, related_name='audio_file')
     video_id = models.OneToOneField(
         VideoFiles, on_delete=models.CASCADE, related_name='audio_file')
-    
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='audios')
+
     def __str__(self):
         return self.name_audio
 
@@ -89,7 +136,8 @@ class AudioFiles(models.Model):
 
             # Подсчет пропускной способности в Mbps
             file_size_in_bytes = os.path.getsize(audio_path)
-            bandwidth = (file_size_in_bytes * 8) / (duration * 1000000)  # Переводим в Mbps
+            bandwidth = (file_size_in_bytes * 8) / \
+                (duration * 1000000)  # Переводим в Mbps
 
             # Обновление поля пропускной способности
             self.bandwidth = bandwidth
@@ -97,4 +145,4 @@ class AudioFiles(models.Model):
 
             return True  # Успешно обновлено
         except FileNotFoundError:
-            return False  # Ошибка: файл не найден 
+            return False  # Ошибка: файл не найден
