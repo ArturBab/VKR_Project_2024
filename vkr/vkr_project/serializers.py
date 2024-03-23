ca5cd782659670d32b2f963bd4b9604980bb0a5f
Артур <90608148+ArturBab@users.noreply.github.com>
2024-03-23 15:45:59 +0300

    Update serializers.py

--- vkr/vkr_project/serializers.py
+++ vkr/vkr_project/serializers.py
@@ -7,8 +7,29 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'name', 'middle_name', 'last_name', 'email', 'password']
+        fields = ['id', 'username', 'name', 'middle_name', 'last_name', 'email', 'password', 'role', 'group']
         extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        role = validated_data.get('role')
+        group = validated_data.get('group')
+        
+        if role == 'Teacher':
+            validated_data.pop('group', None)
+        elif role == 'Student':
+            if not group:
+                raise serializers.ValidationError("Для студента необходимо указать группу.")
+        else:
+            raise serializers.ValidationError("Недопустимая роль.")
+        
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        
+        if password is not None:
+            instance.set_password(password)
+        
+        instance.save()
+        return instance
         
 
 class AudioFilesSerializer(serializers.ModelSerializer):
