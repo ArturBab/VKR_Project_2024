02dbfe159a8cb319095a5c046081021cda3a4fbb
Артур <90608148+ArturBab@users.noreply.github.com>
2024-05-02 15:51:56 +0400

    Update urls.py

--- vkr/settings/urls.py
+++ vkr/settings/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from vkr_project.views.content_views import content_list, update_content, content_delete
 from vkr_project.views.audiofiles_views import audio_files_list, update_audio_file, audio_files_delete
 from vkr_project.views.videofiles_views import video_files_list, update_video_file, video_files_delete
+from vkr_project.views.notifications_views import (create_notification, 
+                                                   mark_group_notifications_as_read, 
+                                                   group_notification_status)
 from django.conf import settings
 from django.conf.urls.static import static
 from vkr_project.views.user_views import *
@@ -11,14 +14,14 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,
 )
-from vkr_project.views.user_views import RegisterAPIView
 from vkr_project.views_templates.templates_views import *
+from rest_framework_simplejwt.views import TokenBlacklistView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/content/', content_list),
+    path('api/content/', content_list, name='api-content_list'),
     path('api/content/update/<int:content_id>/',
          update_content, name='update_content'),
     path('api/content/delete/<int:content_id>/',
@@ -35,15 +38,22 @@ urlpatterns = [
     path('api/video/delete/<int:video_id>/',
          video_files_delete, name='video-files-delete'),
 
+     path('api/notification-create/', create_notification, name='api-notification-create'),
+     path('api/notification-read/', mark_group_notifications_as_read, name='api-notification-read'),
+     path('api/notification-status/', group_notification_status, name='api-notification-status'),   
+
     path('api/register/', RegisterAPIView.as_view(), name='api-register'),
-    path('api/login/', TokenObtainPairView.as_view(), name='login_with_token'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/login/', CustomTokenObtainPairView.as_view(), name='login_with_token'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/user/', CurrentUserAPIView.as_view(), name='api-user'),
+    path('api/logout/', LogoutAPIView.as_view(), name='api-logout'),
     path('api/students_list/', student_list, name='students_list'),
     path('api/groups_list/', list_student_groups, name='list_student_groups'),
     path('api/teachers_list/', teachers_list, name='teachers_list'),
 
     path('', home_view, name='home'),
+    path('register/', register, name='register'),
+    path('login/', login_view, name='login'),
 
 ]
 
