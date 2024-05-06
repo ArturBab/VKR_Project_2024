30abe3371c24756629d3ed59b4ad574694b4900b
Артур <90608148+ArturBab@users.noreply.github.com>
2024-05-06 19:19:27 +0400

    Update templates_views.py

--- vkr/vkr_project/views/views_templates/templates_views.py
+++ vkr/vkr_project/views/views_templates/templates_views.py
@@ -4,8 +4,10 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from ..forms import RegistrationForm
 from django.contrib.auth import logout
 from ..models import User
-from django.http import JsonResponse
 import requests
+from ..views.content_views import content_list
+from django.contrib.auth.decorators import login_required
+from rest_framework.authtoken.models import Token
 
 
 def signup(request):
@@ -35,15 +37,16 @@ def signup(request):
                 'telegram_id': telegram_id
             })
             if response.ok:
-                return JsonResponse({'success': True})
+                return redirect('login')
             else:
-                # Если сервер вернул ошибку, получаем JSON и обрабатываем сообщения об ошибках
                 error_data = response.json()
-                for field, errors in error_data.items():
-                    error_messages[field] = ', '.join(errors)
+                if isinstance(error_data, dict):
+                    for key, value in error_data.items():
+                        error_messages[key] = ', '.join(value)
+                elif isinstance(error_data, list):
+                    error_messages['__all__'] = ', '.join(error_data)
 
         except requests.RequestException as e:
-            # Если произошла ошибка при обращении к серверу, добавляем общее сообщение об ошибке
             error_messages['__all__'] = str(e)
 
     # Если метод запроса не POST или возникли ошибки, возвращаем страницу с формой регистрации
@@ -51,27 +54,73 @@ def signup(request):
 
 
 def login_view(request):
-    message = None
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                # Перенаправление на главную страницу после успешного входа
-                return redirect('home')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            response = requests.post('http://127.0.0.1:8000/api/login/', json={
+                'username': username,
+                'password': password
+            })
+
+            if response.status_code == 200:
+                # Успешно получен токен и данные пользователя
+                access_token = response.json().get('access')
+                refresh_token = response.json().get('refresh')
+
+                # Получаем данные пользователя
+                user_response = requests.get(
+                    'http://127.0.0.1:8000/api/user/', headers={'Authorization': f'Bearer {access_token}'})
+                user_data = user_response.json()
+
+                # Передаем данные пользователя и устанавливаем куки в ответе
+                response = render(request, 'home.html', {'user': user_data})
+                response.set_cookie('access_token', access_token)
+                response.set_cookie('refresh_token', refresh_token)
+
+                return response
+
+            elif response.status_code == 400:
+                # Неправильные учетные данные
+                error_message = "Неправильные учетные данные. Пожалуйста, попробуйте еще раз."
+                return render(request, 'login.html', {'error_message': error_message})
+
             else:
-                message = "Неверное имя пользователя или пароль."
+                # Другие ошибки
+                error_message = "Произошла ошибка. Пожалуйста, попробуйте еще раз."
+                return render(request, 'login.html', {'error_message': error_message})
+
+        except requests.RequestException as e:
+            # Ошибка запроса к API
+            error_message = f"Ошибка запроса к API: {str(e)}"
+            return render(request, 'login.html', {'error_message': error_message})
+
     else:
-        form = AuthenticationForm(data=request.POST)
-    return render(request, 'login.html', {'form': form, 'message': message})
+        return render(request, 'login.html')
 
 
 def base_view(request):
     return render(request, 'base.html')
 
 
+@login_required
 def home_view(request):
-    return render(request, 'home.html')
+    user = request.user
+    print(request.user)
+    return render(request, 'home.html', {'user': user})
+
+
+def display_content_list(request):
+    # Получаем данные из API эндпоинта content_list
+    response = content_list(request)
+
+    # Проверяем, успешно ли получены данные
+    if response.status_code == 200:
+        # Если успешно, передаем данные на шаблон
+        content_data = response.data
+        return render(request, 'content_list.html', {'content_data': content_data})
+    else:
+        # Если произошла ошибка, выводим сообщение об ошибке на шаблон
+        error_message = "Ошибка получения данных."
+        return render(request, 'content_list.html', {'error_message': error_message})
