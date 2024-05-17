49613a2b21e00b2f0d746600128339d8395a7bd6
Артур <90608148+ArturBab@users.noreply.github.com>
2024-05-17 19:35:29 +0300

    Create telegram.py

---
+++ vkr/vkr_project/telegram.py
@@ -0,0 +1,95 @@
+from telegram import Bot
+from telegram.error import TelegramError
+from telegram.ext import *
+import json
+from django.http import JsonResponse
+import requests
+from django.views.decorators.csrf import csrf_exempt
+import requests
+from settings.settings import TELEGRAM_BOT_TOKEN, ADMIN_TELEGRAM_CHAT_ID
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+
+
+
+def notify_server_start():
+    bot_token = TELEGRAM_BOT_TOKEN
+    chat_id = ADMIN_TELEGRAM_CHAT_ID
+    message = "Сервер ИС запущен"
+
+    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+    payload = {
+        'chat_id': chat_id,
+        'text': message
+    }
+
+    response = requests.post(url, json=payload)
+
+    if response.ok:
+        print("Уведомление успешно отправлено.")
+    else:
+        print(f"Не удалось отправить уведомление. Ошибка: {response.text}")
+
+
+def notify_server_stop(request):
+    try:
+        bot_token = TELEGRAM_BOT_TOKEN
+        chat_id = ADMIN_TELEGRAM_CHAT_ID
+        message = "Сервер ИС был приостановлен"
+
+        url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+        payload = {
+            'chat_id': chat_id,
+            'text': message
+        }
+
+        response = requests.post(url, json=payload)
+
+        if response.ok:
+            return JsonResponse({'status': 'success'})
+        else:
+            return JsonResponse({'error': f'Failed to send message to Telegram bot. Error message: {response.text}'}, status=500)
+    except Exception as e:
+        return JsonResponse({'error': f'An error occurred: {str(e)}'}, status=500)
+    
+# --------------------------------------------------------------------------------------#
+
+
+
+# --------------------------------------------------------------------------------------#
+
+
+@csrf_exempt
+def send_telegram_notification(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            chat_id = data.get('chat_id')
+            message = data.get('message')
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+
+        if not chat_id or not message:
+            return JsonResponse({'error': 'chat_id and message are required'}, status=400)
+
+        # Отправляем запрос к Telegram Bot API для отправки сообщения
+        url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
+        params = {
+            'chat_id': chat_id,
+            'text': message
+        }
+        response = requests.post(url, json=params)
+
+        # Проверяем успешность запроса и возвращаем результат
+        if response.ok:
+            return JsonResponse({'status': 'success'})
+        else:
+            error_message = response.json().get('description', 'Unknown error')
+            return JsonResponse({'error': f'Failed to send message to Telegram bot. Error message: {error_message}'}, status=500)
+
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=400)
